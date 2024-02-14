33270cb8701fe73b2619b49e75806b0ca211485e
rzapriono <apriono.reza@gmail.com>
2024-02-14 15:55:35 +0700

    added test for controller

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/HomePageControllerTest.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsString;
+
+@WebMvcTest(HomePageController.class)
+public class HomePageControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void setHomePageControllerTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.view().name("homePage"))
+                .andExpect(MockMvcResultMatchers.content().string(containsString("ADV Shop")));
+    }
+}
