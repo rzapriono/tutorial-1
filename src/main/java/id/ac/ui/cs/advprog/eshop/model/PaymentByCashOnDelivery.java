eab30ce36ffa0531be097622c315ab0e4359a80d
rzapriono <apriono.reza@gmail.com>
2024-03-06 19:19:33 +0700

    [RED] Add PaymentByCashOnDelivery model skeleton

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/PaymentByCashOnDelivery.java
@@ -0,0 +1,18 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import java.util.Map;
+
+public class PaymentByCashOnDelivery extends Payment {
+    public PaymentByCashOnDelivery(String id, Order order, String method, Map<String, String> paymentData, String status) {
+        super(id, method, order, paymentData, status);
+    }
+
+    public PaymentByCashOnDelivery(String id, Order order, String method, Map<String, String > paymentData) {
+        super(id, method, order, paymentData);
+    }
+
+    @Override
+    protected void setPaymentData(Map<String, String> paymentData) {
+
+    }
+}
